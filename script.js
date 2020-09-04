7dcd8164c00840c2c093f369c8beb06368511a8e
Fechulo <fhandl@gmail.com>
2020-09-04 01:29:38 +0100

    Add compositions page

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-function calculate_age(dob) { 
+function calculate_age(dob) {
   var diff_ms = Date.now() - dob.getTime();
-  var age_dt = new Date(diff_ms); 
-  
+  var age_dt = new Date(diff_ms);
+
   return Math.abs(age_dt.getUTCFullYear() - 1970);
 }
 
@@ -12,14 +12,14 @@ function showDropdown() {
 }
 
 // Close the dropdown menu if the user clicks outside of it
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
+window.onclick = function (event) {
+  if (!event.target.matches(".dropbtn")) {
     var dropdowns = document.getElementsByClassName("dropdown-content");
     for (var i = 0; i < dropdowns.length; i++) {
       var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
       }
     }
   }
-}
+};
